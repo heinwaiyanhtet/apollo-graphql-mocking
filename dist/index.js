49d4cc960102d7bae40642b764940132543218ef
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-10-25 10:21:19 +0630

    apollo setup

---
+++ dist/index.js
@@ -0,0 +1,51 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    title: String
+    author: String
+  }
+
+  # The "Query" type lists all the available queries.
+  type Query {
+    books: [Book]
+  }
+
+  # The "Mutation" type lists all the mutations.
+  type Mutation {
+    addBook(title: String!, author: String!): Book
+  }
+`;
+const books = [
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster',
+    },
+];
+const resolvers = {
+    Query: {
+        books: () => books,
+    },
+    Mutation: {
+        addBook: (_, { title, author }) => {
+            const newBook = { title, author };
+            books.push(newBook);
+            return newBook;
+        },
+    },
+};
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+console.log(`🚀  Server ready at: ${url}`);
